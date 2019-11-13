4be7d1ea6fe56d4baf8ada49a4fd0a481213bf0c
Raymond Daniels <rgdwar1@gmx.com>
2019-11-13 11:04:20 -0500

    Initial Commit

---
+++ intro.py
@@ -0,0 +1,41 @@
+# Integers
+print(1+1)
+print(2-1)
+print(2*4)
+print(40/10)
+
+# Strings
+print("John")  # Constants
+name = "RAY"  # Variables
+print(name)
+
+# String Methods
+print(name.upper())  # UPPER
+print(name.lower())  # lower
+print(len(name))  # len - length
+print(name.capitalize())  # Capitalizes the first character
+print(name.count("r"))
+print(name.count("R"))
+
+# Escaping special Characters
+
+# print('Hello, it's nice outside) #Syntax Error
+print('Hello, it\'s nice outside')  # Syntax Error
+
+
+# Variables
+
+# Variable names must follow:
+#       Can start with _ or letter
+#       Can contain _ Letter or number
+
+country = "Germany"
+team = country
+print(team)
+
+#       Can reassign variable values
+part1 = "Trevor"
+part2 = "Mark"
+part1 = "Bernard"
+
+print(part1, part2)
